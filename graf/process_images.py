ad5aa4e404ff9e310b246e9038513cd557933dfa
Sven A. Schmidt <sas@abstracture.de>
2012-02-20 18:25:41 +0100

    Added (dummy) image processing

---
+++ graf/process_images.py
@@ -0,0 +1,52 @@
+import database
+import time
+import logging
+from optparse import OptionParser
+import pypsum
+import random
+
+logger = logging.getLogger('graf.process_images')
+
+
+def process_doc(doc):
+  logger.info("Processing '%s'" % doc.id)
+  count = int(random.uniform(1,4))
+  text = pypsum.get_lipsum(howmany=count, what='words', start_with_lipsum='0')
+  text = text[0]
+  text = ' '.join(text.split()[:count]) # service always returns at least 5 words
+  doc['text_result'] = text
+  doc['processed'] = True
+  return doc
+
+
+def process_images(db):
+  for row in db.unprocessed_docs_view():
+    doc = db[row.id]
+    doc = process_doc(doc)
+    db.save(doc)
+
+
+if __name__ == '__main__':
+  logging.getLogger("graf").addHandler(logging.StreamHandler())
+  logging.getLogger("graf").setLevel(logging.INFO)
+  usage = ("usage: %prog [options]")
+  parser = OptionParser(usage=usage)
+  parser.add_option('-e', '--env', dest='env', default='DEV',
+                    help='database environment (DEV, UAT, PRD,...)')
+  parser.add_option('-l', '--loglevel', dest='loglevel',
+                    default='info',
+                    help='set loglevel INFO, WARNING, DEBUG')
+  (options, args) = parser.parse_args()
+
+  options.loglevel = options.loglevel.upper()
+  if options.loglevel=='WARNING':
+    logging.getLogger("graf").setLevel(logging.WARNING)
+  elif options.loglevel=='DEBUG':
+    logging.getLogger("graf").setLevel(logging.DEBUG)
+
+  logger.info('Starting image processor')
+  db = database.connect(options.env)
+  while True:
+    process_images(db)
+    time.sleep(0.5)
+  
---
+++ tests/test_02_process_images.py
@@ -0,0 +1,40 @@
+from nose.tools import ok_, eq_, raises
+
+from graf.database import connect, create_database, drop_database
+from graf import process_images
+
+env = 'TEST'
+
+class TestProcessImages(object):
+  
+  def setUp(self):
+    # TEMP
+    drop_database(env)
+
+    create_database(env)
+    self.db = connect(env)
+    self.db.check_views()
+  
+  
+  def tearDown(self):
+    # drop_database(env)
+    pass
+  
+  
+  def test_01_process_images(self):
+    
+    created_at = '2012-02-20T00:%02d:00Z'
+    for i in range(10):
+      key = 'doc_%d' % i
+      doc = {
+        'created_at' : created_at % i,
+      }        
+      self.db[key] = doc
+
+    res = self.db.unprocessed_docs_view()
+    eq_(len(res), 10)
+
+    process_images.process_images(self.db)
+    
+    res = self.db.unprocessed_docs_view()
+    eq_(len(res), 0)
