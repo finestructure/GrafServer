0872a3a027ddcda80c763628b4c5bfd0283f62d4
Sven A. Schmidt <sas@abstracture.de>
2012-02-20 14:02:30 +0100

    Added check_views

--- graf/database.py
+++ graf/database.py
@@ -36,5 +36,80 @@ class Handle(object):
       raise DatabaseNotFound(dbname)
 
 
+class Database(object):
+
+  def __init__(self, db):
+    self.db = db
+
+  design_name = '_design/graf'
+  version = 10
+  views = {
+    'unprocessed_docs': {
+      'map' : """
+        function(doc) {
+          if (! doc.processed) {
+            emit(doc._id, null);
+          }
+        }
+      """
+    },
+    'doc_created_at' : {
+      'map' : """
+        function(doc) {
+          if (doc.created_at) {
+            emit(doc.created_at, [doc._id, doc.source_device, doc.version]);
+          }
+        }
+      """
+    },
+  }
+
+  def check_views(self, recreate=False):
+    if recreate:
+      # delete previous version of views
+      try:
+        del self.db[self.design_name]
+      except couchdb.ResourceNotFound:
+        pass
+    design = self.db.get(self.design_name)
+    if design is None:
+      # create view
+      design = {
+        'views' : self.views,
+        'version' : self.version
+      }
+      self.db[self.design_name] = design
+    elif design['version'] < self.version:
+      # update view
+      design['views'] = self.views
+      design['version'] = self.version
+      self.db[self.design_name] = design
+
+
+
+  def __getitem__(self, key):
+    """
+    Dict-like accessor routed through to couchdb.Database
+    """
+    try:
+      return self.db[key]
+    except couchdb.ResourceNotFound:
+      raise DocumentNotFound(key)
+  
+
+  def __setitem__(self, key, value):
+    """
+    Dict-like accessor routed through to couchdb.Database
+    """
+    self.db[key] = value
+  
+
+  def __getattr__(self, name):
+    """
+    By default route all attributes through to couchdb.Database
+    """
+    return getattr(self.db, name)
+
+
 
 
--- tests/test_01_database.py
+++ tests/test_01_database.py
@@ -2,7 +2,7 @@ from nose.tools import ok_, eq_, raises
 import couchdb
 
 from graf import config
-from graf.database import Handle
+from graf.database import Handle, Database
 
 env = 'TEST'
 
@@ -29,3 +29,20 @@ class TestConnection(object):
     def check():
       self.server[dbname]
     check()
+    
+    
+  def test_03_check_views(self):
+    # setup
+    dbname = config.get(env, 'dbname')
+    self.handle.create_database(dbname)
+    db = self.handle.connect(dbname)
+    db.check_views()
+    # test
+    design = db.get('_design/graf')
+    ok_(design)
+    ok_(design['views'])
+    eq_(Database.version, design['version'])
+    # cleanup
+    self.handle.drop_database(dbname)
+
+
